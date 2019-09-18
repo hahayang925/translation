c58be0c42dc1d43126f532bb5cd99b831badf089
Kim <kim.yang@waveo.com>
2019-09-18 22:45:23 +0800

    Refactor App.js & LanguageContext

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,3 +1,23 @@
 import React from 'react';
 
-export default React.createContext('zh-TW'); 
+// export default React.createContext('zh-TW'); 
+
+const Context = React.createContext('zh-TW');
+
+export class LanguageStore extends React.Component {
+  state = { language: 'zh-TW'};
+
+  onLanguageChange = (language) => {
+    this.setState({language});
+  }
+
+  render() {
+    return (
+      <Context.Provider value={{ ...this.state, onLanguageChange: this.onLanguageChange }}>
+        {this.props.children}
+      </Context.Provider>
+    )
+  }
+}
+
+export default Context;
