9f55919fe53362f41cdc8ec0f0b3954953fc5004
Kim <kim.yang@waveo.com>
2019-09-18 22:50:48 +0800

    Refactor Button, Field & LanguageSelector

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,15 +4,15 @@ import ColorContext from '../contexts/ColorContext';
 
 class Button extends React.Component {
   // static contextType = LanguageContext;
-  renderSubmit(value) {
-    return value === 'en' ? 'Submit' : '送出';
+  renderSubmit(language) {
+    return language === 'en' ? 'Submit' : '送出';
   }
 
   renderButton(color) {
     return (
       <button className={`ui button ${color}`}>
         <LanguageContext.Consumer>
-          {(value) => this.renderSubmit(value)} 
+          {({ language }) => this.renderSubmit(language)} 
         </LanguageContext.Consumer>
       </button>
     )
--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class LanguageSelector extends React.Component {
+  static contextType = LanguageContext;
+
   render() {
     return (
       <div>
         Select a language:
-        <i className="flag us" onClick={() => this.props.onLanguageChange('en')}></i>
-        <i className="flag tw" onClick={() => this.props.onLanguageChange('zh-TW')}></i>
+        <i className="flag us" onClick={() => this.context.onLanguageChange('en')}></i>
+        <i className="flag tw" onClick={() => this.context.onLanguageChange('zh-TW')}></i>
       </div>
     )
   }
